9f0f0e177c33af81bd04f432726c2eec8bfba5e9
Darragh <darra@DESKTOP-FMT3D5T>
2017-02-28 15:56:04 +0000

    Fitness Tests done & Web form added

---
+++ TestingAssignment/CalculatePremium/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CalculatePremium.Global" Language="C#" %>
---
+++ TestingAssignment/FitTest/FitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingAssignment;
+
+namespace FitTest
+{
+    public class FitTest : fit.ColumnFixture
+    {
+        public CalculatePremium cp = new CalculatePremium();
+
+        public string gender;
+        public int age;
+
+        public double CalculatePremium()
+        {
+            return cp.CalcPremium(age, gender);
+        }
+
+    }
+}
